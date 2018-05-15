1cb34d2112b05f2260bddc9acdcb1894aa77792b
Sourabh Ukkalgaonkar <sourabh.ukkalgaonkar@gmail.com>
2018-05-15 18:57:17 +0530

    Display youtube videos

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YouTubSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details'
 
 const API_KEY = 'AIzaSyCZttceDVlkCDFZGSsEcsr1JGkmm2CZ2ZA';
-YouTubSearch({ key: API_KEY, term: 'surfboard' }, function(data){
-  console.log(data);
-});
 
+class App extends Component {
+  constructor(props){
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
-};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YouTubSearch({ key: API_KEY, term: 'surfboard' }, (videos) =>{
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[4]
+      });
+    });
+  }
+
+  render (){
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos}/>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
